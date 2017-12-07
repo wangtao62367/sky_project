9383262518e976f5c78fe39068446923d36d5944
wangtao62367 <623672780@qq.com>
2017-12-07 17:20:07 +0800

    2017120702

---
+++ advanced/backend/controllers/PublicController.php
@@ -0,0 +1,29 @@
+<?php
+namespace backend\controllers;
+
+
+
+use common\controllers\CommonController;
+
+class PublicController extends CommonController
+{
+    
+    
+    public function actionLogin()
+    {
+        
+    }
+    
+    public function actionLogout()
+    {
+        
+    }
+    
+    public function actionFindpwdByMail()
+    {
+        
+    }
+    
+    
+    
+}
